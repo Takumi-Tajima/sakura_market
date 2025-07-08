2dd3407fae30c5f46c1392b1c3b53b9e2dd923d7
taji <taji@sonicgarden.jp>
2025-07-08 13:49:58 +0900

    foodに画像を登録できる

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,8 +1,11 @@
 class Food < ApplicationRecord
+  has_one_attached :cover_image
+
   validates :name, presence: true
   validates :description, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :published, inclusion: { in: [true, false] }
+  validates :cover_image, content_type: %i[png jpg jpeg], size: { less_than: 5.megabytes }
 
   scope :default_order, -> { order(id: :desc) }
 
