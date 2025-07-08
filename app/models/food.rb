24f131ffc960ab0984780204e03745f5a9d97b52
taji <taji@sonicgarden.jp>
2025-07-08 11:59:55 +0900

    税計算追加

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,4 +5,8 @@ class Food < ApplicationRecord
   validates :published, inclusion: { in: [true, false] }
 
   scope :default_order, -> { order(id: :desc) }
+
+  def price_with_tax
+    BigDecimal(price) * BigDecimal(TaxRate.default.to_s)
+  end
 end
---
+++ app/models/tax_rate.rb
@@ -0,0 +1,7 @@
+class TaxRate
+  STANDARD = 1.10
+
+  def self.default
+    STANDARD
+  end
+end
