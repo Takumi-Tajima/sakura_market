ab92b219f66a6e623a2c92544164324ae95c046a
taji <taji@sonicgarden.jp>
2025-07-09 17:57:52 +0900

    公開されている食品のみを追加する

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,10 +4,19 @@ class CartItem < ApplicationRecord
 
   validates :food_id, uniqueness: { scope: :cart_id }
   validates :quantity, numericality: { greater_than: 0 }
+  validate :validate_food_is_published
 
   scope :default_order, -> { order(id: :desc) }
 
   def total_price
     quantity * food.price_with_tax
+  end
+
+  private
+
+  def validate_food_is_published
+    unless food.published?
+      errors.add(:cart_item, :validate_food_is_published)
+    end
   end
 end
