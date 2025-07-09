0398abc92a03241f6a75af8242d56340da677fcb
taji <taji@sonicgarden.jp>
2025-07-09 18:24:02 +0900

    n+1解消

--- app/controllers/users/carts_controller.rb
+++ app/controllers/users/carts_controller.rb
@@ -1,6 +1,5 @@
 class Users::CartsController < Users::ApplicationController
   def show
-    # TODO: n+1の修正
-    @cart_items = current_cart.cart_items.includes(food: [:cover_image_attachment]).default_order
+    @cart_items = current_cart.cart_items.includes(food: :cover_image_attachment).default_order
   end
 end
