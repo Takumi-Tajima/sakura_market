f091e6931abd05a11dae10e0bac787cac208dc9c
taji <taji@sonicgarden.jp>
2025-07-11 13:38:24 +0900

    order代引き手数料リファクタ

--- app/models/order.rb
+++ app/models/order.rb
@@ -17,37 +17,75 @@ class Order < ApplicationRecord
 
   def save_with_cart_items(cart)
     return false if cart.cart_items.empty?
+    return false if invalid?
 
     transaction do
-      self.total_amount = cart.total_price
-      self.item_total_amount = cart.total_price_with_tax
-      self.tax_amount = cart.total_tax_amount
-      self.shipping_fee = 1
-      self.cash_on_delivery_fee = 1
-      self.ordered_at = Time.current
-      self.delivery_on = Date.current
-      self.delivery_time_slot = '未定'
-      self.delivery_name = user.name
-      self.delivery_address = user.address
+      build_order_items_from_cart(cart)
+      assign_attributes_from_cart(cart)
       save!
-      create_order_items(cart)
-      cart.cart_items.destroy_all
-      true
+      cart.destroy!
     end
+    true
   rescue ActiveRecord::RecordInvalid => e
     Rails.logger.error "Order save failed: #{e.message}"
     false
   end
 
+  def calculate_cash_on_delivery_fee(total_amount)
+    case total_amount
+    when 0...10_000
+      300
+    when 10_000...30_000
+      400
+    when 30_000...100_000
+      600
+    else
+      1_000
+    end
+  end
+
   private
 
-  def create_order_items(cart)
+  def build_order_items_from_cart(cart)
     cart.cart_items.each do |cart_item|
-      order_items.create!(
+      order_items.build(
         food: cart_item.food,
         quantity: cart_item.quantity,
         price: cart_item.food.price_with_tax
       )
+    end
+  end
+
+  def assign_attributes_from_cart(cart)
+    self.item_total_amount = cart.total_price_with_tax
+    self.shipping_fee = 1 # TODO: 送料計算実装予定
+    self.cash_on_delivery_fee = calculate_cash_on_delivery_fee(cart.total_price_with_tax)
+
+    calculate_tax_and_total_amounts(cart)
+    assign_delivery_attributes
+  end
+
+  def calculate_tax_and_total_amounts(cart)
+    # 税額計算: 商品税額 + (送料 + 代引き手数料) * 10%
+    shipping_and_delivery_tax = calculate_shipping_and_delivery_tax
+    self.tax_amount = cart.total_tax_amount + shipping_and_delivery_tax
+
+    # 総合計額 = 商品合計(税込) + 送料(税抜) + 代引き手数料(税抜) + (送料+代引き手数料)の税額
+    self.total_amount = item_total_amount + shipping_fee + cash_on_delivery_fee + shipping_and_delivery_tax
+  end
+
+  def calculate_shipping_and_delivery_tax
+    ((shipping_fee + cash_on_delivery_fee) * 0.1).floor
+  end
+
+  def assign_delivery_attributes
+    self.ordered_at = Time.current
+    self.delivery_on = Date.current
+    self.delivery_time_slot = '未定'
+    self.delivery_name = user.name
+    self.delivery_address = user.address
+  end
+
   def validate_user_have_address
     if user.address.blank?
       errors.add(:base, :validate_user_have_address)
