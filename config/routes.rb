6d0198c7a6e7b6d29254b79a6ecb6329d59c5a0e
taji <taji@sonicgarden.jp>
2025-07-15 07:30:49 +0900

    管理者パスワードリセット

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :admins, controllers: { sessions: 'admins/sessions' }
+  devise_for :admins, controllers: { sessions: 'admins/sessions', passwords: 'admins/passwords' }
 
   root 'foods#index'
 
